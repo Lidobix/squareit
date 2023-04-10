8cd9050b0327e33e167a6f2e715b0a6ae8086975
Lidobix <pipoflutepouet@gmail.com>
2023-04-10 22:49:25 +0200

    MaJ module DB

--- public/modules/dbInteractions.js
+++ public/modules/dbInteractions.js
@@ -18,14 +18,14 @@ export async function connectionToDB() {
   }
 }
 
-export async function findSomething(query, projection) {
+export async function findPlayer(player) {
   let mongoClient;
 
   try {
     mongoClient = await connectionToDB();
     const db = mongoClient.db(process.env.DB);
     const collecion = db.collection(process.env.COLLECTION);
-    return await collecion.find(query, projection).toArray();
+    return await collecion.findOne(player).toArray();
   } catch (error) {
     console.error('echec de la récupération de dats', error);
   } finally {
