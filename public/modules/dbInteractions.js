8ef27ebb3a9e623b91d439fe91aa955bcd8fa2d9
Lidobix <pipoflutepouet@gmail.com>
2023-04-10 22:47:50 +0200

    implémentation module DBinteraction

---
+++ public/modules/dbInteractions.js
@@ -0,0 +1,34 @@
+import { MongoClient } from 'mongodb';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export async function connectionToDB() {
+  let mongoClient;
+
+  try {
+    mongoClient = new MongoClient(process.env.DBURL);
+    console.log('connexion à mongo en cours...');
+    await mongoClient.connect();
+    console.log('connecté à mongo');
+    return mongoClient;
+  } catch (error) {
+    console.error('connexion non établie', error);
+    process.exit();
+  }
+}
+
+export async function findSomething(query, projection) {
+  let mongoClient;
+
+  try {
+    mongoClient = await connectionToDB();
+    const db = mongoClient.db(process.env.DB);
+    const collecion = db.collection(process.env.COLLECTION);
+    return await collecion.find(query, projection).toArray();
+  } catch (error) {
+    console.error('echec de la récupération de dats', error);
+  } finally {
+    await mongoClient.close();
+  }
+}
